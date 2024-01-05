a2d336cb620c5bfd20b7f3159bdd06978b8429e4
Jake <jwrunge@gmail.com>
2024-01-04 23:57:37 -0600

    Update configs and fix copyout script

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,6 @@
 require("core.mappings")
 require("core.plugins")
-require("core.options")
+-- require("core.options")
+
+local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
+vim.cmd.source(vimrc)
