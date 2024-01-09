faa32859dd2edd064fe5bbfe04a54a3a4ab605fd
Jake <jwrunge@gmail.com>
2024-01-08 22:51:16 -0600

    Update to lazyvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,2 @@
-require("core.mappings")
-require("core.plugins")
--- require("core.options")
-
-local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
-vim.cmd.source(vimrc)
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
